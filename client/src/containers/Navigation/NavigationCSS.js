b138728362eeed0e54c6ad406b1e019e44849f14
Michal Klauza <klauza.uk@gmail.com>
2020-06-13 15:28:15 +0100

    create nav layout

--- client/src/containers/Navigation/NavigationCSS.js
+++ client/src/containers/Navigation/NavigationCSS.js
@@ -1,18 +1,85 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 50px; height: 100%;
-  background: #fff;
-
   position: fixed;
-  left: 0;
-  top: 0; bottom: 0;
+  left: 0; right: 0;
+  bottom: 0;
+  z-index: 2020;
 
-  @media(max-width: 768px){
-    width: 100%; height: 50px;
+  width: 100%; height: 50px;
+  background: #d4d4d4;
 
-    top: unset;
-    left: 0; right: 0;
-    bottom: 0;
+
+
+  .nav-container{
+    display: flex; flex-direction: row;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  .nav{
+    &--element{
+      border: 1px solid #000;
+      display: flex; flex-direction: column;
+      border: 1px solid yellow;
+    }
+  }
+
+
+  .navlink-2{
+    position: relative;
+
+    .articles-group{
+      width: 200px;
+      background: red;
+      display: flex; flex-direction: row;
+      justify-content: space-between;
+      text-align: center;
+
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateY(-100%) translateX(-50%);
+      opacity: 0;
+      display: none;
+      &.toggle{
+        display: block;
+        opacity: 1;
+      }
+      svg{
+        margin: 0 auto;
+      }
+      a{ 
+        padding: 5px;
+        width: 100%;
+      }
+
+    }
+
+
+  }
+
+  @media ${props => props.theme.device.laptop}{
+    width: 50px; height: 100%;
+    left: 0;
+    top: 0; bottom: 0;
+
+    .nav-container{
+      flex-direction: column;
+      justify-items: center;
+      align-items: center;
+    }
+
+    .navlink-2{
+      position: relative;
+  
+      .articles-group{
+        top: 0;
+        left: 100%;
+        transform: translateY(0) translateX(0);
+      }
+    }
+  
   }
 `;
--- client/src/containers/Navigation/index.js
+++ client/src/containers/Navigation/index.js
@@ -1,33 +1,91 @@
-import React, { useContext } from 'react'
+import React, { useRef, useState, useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+
+// other
 import Switch from 'react-switch';
+import { useSpring, animated } from 'react-spring';
+
+// media & css
 import { ThemeContext } from 'styled-components';
 import { HomeIcon } from '../../media/Icons';
-
 import { Wrapper } from './NavigationCSS';
 
+
+// ---
 const Navigation = ({ toggleTheme }) => {
+
+  const articlesGroup = useRef();
 
   const { colors, title } = useContext(ThemeContext);
 
+  const [toggle, setToggle] = useState(false);
+
+  // const props = useSpring({opacity: toggle ? 1 : 0 })
+
+  const toggleNavGroup = (e) => {
+    articlesGroup.current.classList.toggle('toggle');
+    // e.stopPropagation();
+    // setToggle(!toggle);
+  }
+
   return (
+    <>
+
     <Wrapper>
+      <div className="nav nav-container">
+        <NavLink to="/" className="nav--element navlink-1">
+          <HomeIcon style={{width: "25px", height: "25px"}} />
+          <span>Home</span>
+        </NavLink>
 
-      <HomeIcon style={{width: "25px", height: "25px"}} />
-      NAVIGATION
-      <Switch 
-        onChange={toggleTheme}
-        checked={title === 'dark'}
-        checkedIcon={false}
-        uncheckedIcon={false}
-        height={10}
-        width={40}
-        handleDiameter={20}
-        onColor={colors.secondary}
-        offColor="#ff0000"
-      />
+        <div className="nav--element navlink-2" 
+          // onTouchEnd={eventNav}
+          onPointerEnter={toggleNavGroup}
+          onPointerLeave={toggleNavGroup}
 
+        >
+          <HomeIcon style={{width: "25px", height: "25px"}} />
+          <span>Articles</span>
 
+          <animated.div className="articles-group" ref={articlesGroup}>
+
+            <NavLink to="/articles/all">
+              <HomeIcon style={{width: "25px", height: "25px"}} />
+              <span>All</span>
+            </NavLink>
+
+            <NavLink to="/articles/programming">
+              <HomeIcon style={{width: "25px", height: "25px"}} />
+              <span>Programming</span>
+            </NavLink>
+
+            <NavLink to="/articles/other">
+              <HomeIcon style={{width: "25px", height: "25px"}} />
+              <span>Other</span>
+            </NavLink>
+
+          </animated.div>
+
+          
+          
+        </div>
+        
+
+        <Switch 
+          onChange={toggleTheme}
+          checked={title === 'dark'}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          height={10}
+          width={40}
+          handleDiameter={20}
+          onColor={colors.secondary}
+          offColor="#ff0000"
+        />
+
+      </div>
     </Wrapper>
+    </>
   )
 }
 
