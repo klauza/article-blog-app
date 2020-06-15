1fc672987b4c9ba5dbddc13d92fba5dec2bf120a
Michal Klauza <klauza.uk@gmail.com>
2020-06-15 20:32:28 +0100

    add a "single article" dynamic route

---
+++ client/src/views/pages/Article/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// css
+import { Wrapper } from './ArticleCSS';
+
+const Article = () => {
+  return (
+    <Wrapper className="page">
+      <h1>Single article</h1>
+    </Wrapper>
+  )
+}
+
+export default Article
--- client/src/views/pages/Home/FeaturedArticles.js
+++ client/src/views/pages/Home/FeaturedArticles.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { NavLink } from 'react-router-dom';
 import f_articles from '../../../data/featuredArticles';
 
 // css
@@ -8,17 +9,19 @@ const FeaturedArticles = () => {
   return (
     <FeaturedArticlesContainer>
       <h1>Featured articles</h1>
-      
+
+        
       <div className="container">
         {f_articles.map((a, i) => (
-          <div key={i} className="card"> 
+          <NavLink to={a.link} key={i} className="card"> 
             <div className="card-title"><h3>{a.name}</h3></div>
             <div className="imgBox">
               <img src={a.img} alt="" />
             </div>
-          </div>
+          </NavLink>
         ))}
       </div>
+
 
     </FeaturedArticlesContainer>
   )
